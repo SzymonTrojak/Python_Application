090d8b394075645a771111c751f8542f40c7e971
SzymonTrojak <96496093+SzymonTrojak@users.noreply.github.com>
2024-07-01 17:54:36 +0200

    Delete zajecia06 directory

--- zajecia06/operations/__init__.py
+++
@@ -1,5 +0,0 @@
-from .incident import Incident
-from .incident_queue import IncidentQueue
-from .asigning_ambulances import asign_ambulances
-
-__all__ = ["Incident", "IncidentQueue", "asign_ambulances"]
